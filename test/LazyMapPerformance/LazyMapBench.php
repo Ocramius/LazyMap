3f16ead7f4956e4e9cc320d895a690a60199a06f
Marco Pivetta <ocramius@gmail.com>
2019-08-11 03:57:19 +0200

    Switch from Athletic to PHPBench

---
+++ test/LazyMapPerformance/LazyMapBench.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LazyMapPerformance;
+
+use LazyMapTestAsset\NullArrayBasedLazyMap;
+use LazyMapTestAsset\NullLazyMap;
+use PhpBench\Benchmark\Metadata\Annotations\BeforeMethods;
+use function array_key_exists;
+
+/**
+ * @BeforeMethods({"setUp"})
+ */
+class LazyMapBench
+{
+    /** @var array<string, int> */
+    private $array;
+
+    /** @var NullArrayBasedLazyMap */
+    private $arrayMap;
+
+    /** @var NullLazyMap */
+    private $lazyMap;
+
+    public function setUp() : void
+    {
+        $this->array    = ['existingKey' => 0];
+        $this->arrayMap = new NullArrayBasedLazyMap();
+        $this->lazyMap  = new NullLazyMap();
+
+        // enforcing key initialization
+        $this->arrayMap->get('existingKey');
+        $this->lazyMap->existingKey;
+    }
+
+    public function benchInitializedArrayPerformance() : int
+    {
+        if (array_key_exists('existingKey', $this->array)) {
+            return $this->array['existingKey'];
+        }
+
+        return 0;
+    }
+
+    public function benchInitializedArrayMapPerformance() : int
+    {
+        return $this->arrayMap->get('existingKey');
+    }
+
+    /** @return null */
+    public function benchInitializedLazyMapPerformance()
+    {
+        return $this->lazyMap->existingKey;
+    }
+
+    public function benchUnInitializedArrayPerformance() : int
+    {
+        if (array_key_exists('nonExistingKey', $this->array)) {
+            return $this->array['nonExistingKey'];
+        }
+
+        return $this->array['nonExistingKey'] = 0;
+    }
+
+    public function benchUnInitializedArrayMapPerformance() : int
+    {
+        return $this->arrayMap->get('nonExistingKey');
+    }
+
+    /** @return null */
+    public function benchUnInitializedLazyMapPerformance()
+    {
+        return $this->lazyMap->nonExistingKey;
+    }
+}
