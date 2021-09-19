c9474c667c58b0fbeccec42569ddc4f2a7f2599a
Marco Pivetta <ocramius@gmail.com>
2021-09-19 03:48:32 +0200

    Upgraded coding standard, mutation testing tooling and benchmark dependencies

--- src/LazyMap/AbstractLazyMap.php
+++ src/LazyMap/AbstractLazyMap.php
@@ -13,8 +13,8 @@ abstract class AbstractLazyMap
      * Magic PHP getter {@link http://www.php.net/manual/en/language.oop5.overloading.php#object.get}
      *
      * @return mixed reference to the instantiated property
-     *
      * @psalm-return T
+     *
      * @psalm-suppress MixedInferredReturnType
      */
     public function & __get(string $name)
@@ -29,7 +29,6 @@ abstract class AbstractLazyMap
      * Instantiate a particular key by the given name
      *
      * @return mixed
-     *
      * @psalm-return T
      */
     abstract protected function instantiate(string $name);
